0e931e90dcad8555fdccccda13009313c88d3b49
Akiyama <mail@msis-net.com>
2024-01-19 14:47:26 +0900

    main

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -6,8 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  compressHTML: false,
-  //HTMLを圧縮しない
+  compressHTML: false, //HTMLを圧縮しない
+  output: 'static',
   build: {
     // Example: Generate `page.html` instead of `page/index.html` during build.
     format: 'file'
